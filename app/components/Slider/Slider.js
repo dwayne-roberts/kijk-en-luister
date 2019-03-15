0d338a7d103337d36f31e8a28836edc6d9d1bc40
Dwayne Roberts <dwayne.roberts@MacBook-Pro-van-Dwayne.local>
2019-03-15 13:33:36 +0100

    Add Slider component

---
+++ app/components/Slider/Slider.js
@@ -0,0 +1,38 @@
+import React, { PureComponent } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+
+export default class Slider extends PureComponent {
+  static defaultProps = {};
+  static propTypes = {};
+
+  render() {
+    return (
+      <Carousel>
+        <div>
+          <img src="https://placekitten.com/1280/600" alt="" />
+          <h6>Legend 1</h6>
+        </div>
+        <div>
+          <img src="https://placekitten.com/1280/600" alt="" />
+          <h6>Legend 1</h6>
+        </div>
+        <div>
+          <img src="https://placekitten.com/1280/600" alt="" />
+          <h6>Legend 1</h6>
+        </div>
+        <div>
+          <img src="https://placekitten.com/1280/600" alt="" />
+          <h6>Legend 1</h6>
+        </div>
+        <div>
+          <img src="https://placekitten.com/1280/600" alt="" />
+          <h6>Legend 1</h6>
+        </div>
+        <div>
+          <img src="https://placekitten.com/1280/600" alt="" />
+          <h6>Legend 1</h6>
+        </div>
+      </Carousel>
+    );
+  }
+}
